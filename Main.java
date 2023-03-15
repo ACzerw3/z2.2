4ee224ae14ab2f5bd140d9705ca60c8c77603c21
ACzerw3 <126687767+ACzerw3@users.noreply.github.com>
2023-03-15 13:24:28 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+import org.hamcrest.core.IsInstanceOf;
+
+class Main {
+  public static void main(String[] args) 
+  {
+    int x=1;
+    for (int i=0; i<50; i++){
+      System.out.println(x);
+      x=x+2;}       
+  }
+  }
